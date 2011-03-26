d25b107454936f1775310bfaa4d0459ed9856378
Alessandro Rubini <rubini@gnudd.com>
2011-03-27 00:01:04 +0100

    PTPWRd/ptpd_exports.c: removed unneeded #includes

--- PTPWRd/ptpd_exports.c
+++ PTPWRd/ptpd_exports.c
@@ -1,12 +1,12 @@
-#include <stdio.h>
-#include <stdlib.h>
+//#include <stdio.h>
+//#include <stdlib.h>
 //#include <string.h>
 
-#include <math.h>
+//#include <math.h>
 
 
-#include <inttypes.h>
-#include <sys/time.h>
+//#include <inttypes.h>
+//#include <sys/time.h>
 
 #include <wr_ipc.h>
 
