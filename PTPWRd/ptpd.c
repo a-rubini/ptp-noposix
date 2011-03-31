d944d65e8970a628b5a85a0a901377d99be08d58
Alessandro Rubini <rubini@gnudd.com>
2011-03-31 16:04:16 +0200

    PTPWRd/ptpd.c: change init of rtdata to save space (400b)

--- PTPWRd/ptpd.c
+++ PTPWRd/ptpd.c
@@ -2,49 +2,48 @@
 
 #include "ptpd.h"
 
-RunTimeOpts rtOpts;  /* statically allocated run-time configuration data */
-
-void init_default_rtopts(RunTimeOpts *rto)
-{
-   rto->announceInterval = DEFAULT_ANNOUNCE_INTERVAL;
-   rto->syncInterval = DEFAULT_SYNC_INTERVAL;
-   rto->clockQuality.clockAccuracy = DEFAULT_CLOCK_ACCURACY;
-   rto->clockQuality.clockClass = DEFAULT_CLOCK_CLASS;
-   rto->clockQuality.offsetScaledLogVariance = DEFAULT_CLOCK_VARIANCE;
-   rto->priority1 = DEFAULT_PRIORITY1;
-   rto->priority2 = DEFAULT_PRIORITY2;
-   rto->domainNumber = DEFAULT_DOMAIN_NUMBER;
-   rto->slaveOnly = SLAVE_ONLY;
-   rto->currentUtcOffset = DEFAULT_UTC_OFFSET;
-   rto->noResetClock = DEFAULT_NO_RESET_CLOCK;
-   rto->noAdjust = NO_ADJUST;
-   rto->inboundLatency.nanoseconds = DEFAULT_INBOUND_LATENCY;
-   rto->outboundLatency.nanoseconds = DEFAULT_OUTBOUND_LATENCY;
-   rto->s = DEFAULT_DELAY_S;
-   rto->ap = DEFAULT_AP;
-   rto->ai = DEFAULT_AI;
-   rto->max_foreign_records = DEFAULT_MAX_FOREIGN_RECORDS;
+/* Statically allocated run-time configuration data.
+ * This should better be a set of functions, as some could be
+ * defined as "const" for freestanding code, thus saving space in
+ * associated code (if() are known at compile time).
+ * Currently, I only removed the init code to compile-time stuff
+ * (bss-ing it to then copy values is bigger than precompiling) -- ARub
+ */
+RunTimeOpts rtOpts = {
+   .announceInterval = DEFAULT_ANNOUNCE_INTERVAL,
+   .syncInterval = DEFAULT_SYNC_INTERVAL,
+   .clockQuality.clockAccuracy = DEFAULT_CLOCK_ACCURACY,
+   .clockQuality.clockClass = DEFAULT_CLOCK_CLASS,
+   .clockQuality.offsetScaledLogVariance = DEFAULT_CLOCK_VARIANCE,
+   .priority1 = DEFAULT_PRIORITY1,
+   .priority2 = DEFAULT_PRIORITY2,
+   .domainNumber = DEFAULT_DOMAIN_NUMBER,
+   .slaveOnly = SLAVE_ONLY,
+   .currentUtcOffset = DEFAULT_UTC_OFFSET,
+   .noResetClock = DEFAULT_NO_RESET_CLOCK,
+   .noAdjust = NO_ADJUST,
+   .inboundLatency.nanoseconds = DEFAULT_INBOUND_LATENCY,
+   .outboundLatency.nanoseconds = DEFAULT_OUTBOUND_LATENCY,
+   .s = DEFAULT_DELAY_S,
+   .ap = DEFAULT_AP,
+   .ai = DEFAULT_AI,
+   .max_foreign_records = DEFAULT_MAX_FOREIGN_RECORDS,
 
    /**************** White Rabbit *************************/
-   rto->portNumber 		= NUMBER_PORTS;
-   rto->wrNodeMode 		= NON_WR;
-   rto->calibrationPeriod     = WR_DEFAULT_CAL_PERIOD;
-   rto->calibrationPattern    = WR_DEFAULT_CAL_PATTERN;
-   rto->calibrationPatternLen = WR_DEFAULT_CAL_PATTERN_LEN;
-   rto->E2E_mode 		= TRUE;
+   .portNumber 		= NUMBER_PORTS,
+   .wrNodeMode 		= NON_WR,
+   .calibrationPeriod     = WR_DEFAULT_CAL_PERIOD,
+   .calibrationPattern    = WR_DEFAULT_CAL_PATTERN,
+   .calibrationPatternLen = WR_DEFAULT_CAL_PATTERN_LEN,
+   .E2E_mode 		= TRUE,
    /********************************************************/
-
-}
-
+};
 
 int main(int argc, char **argv)
 {
    PtpClock *ptpClock;
    Integer16 ret;
    int i;
-   /* initialize run-time options to default values */
-
-   init_default_rtopts(&rtOpts);
 
    netStartup();
 
