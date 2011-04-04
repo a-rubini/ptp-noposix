2d5e8018f4d4b08ae9bc95e59fa84790c3a8a0d4
Tomasz Wlostowski <tomasz.wlostowski@cern.ch>
2011-04-05 00:41:28 +0200

    updated hal_exports.h from HAL SVN

--- wrsw_hal/hal_exports.h
+++ wrsw_hal/hal_exports.h
@@ -1,7 +1,7 @@
 #ifndef __HAL_EXPORTS_H
 #define __HAL_EXPORTS_H
 
-//#include <inttypes.h>  -- now in ptpd-wrappers.h
+#include <inttypes.h>
 
 #define HAL_MAX_PORTS 64
 
@@ -41,40 +41,62 @@
 #define HEXP_ON 1
 #define HEXP_OFF 0
 
+#define HEXP_HPLL 0
+#define HEXP_DMPLL 1
+
+#define HEXP_FREQ 0
+#define HEXP_PHASE 1
+
 
 typedef struct {
 
   char port_name[16];
 
- uint32_t current_phase_shift;
- int32_t adjust_phase_shift;
+  uint32_t current_phase_shift;
+  int32_t adjust_phase_shift;
 
- int64_t adjust_utc;
- int32_t adjust_nsec;
+  int64_t adjust_utc;
+  int32_t adjust_nsec;
  
- uint64_t current_utc;
- uint32_t current_nsec;
+  uint64_t current_utc;
+  uint32_t current_nsec;
  
 } hexp_pps_params_t;
 
+/* Port modes (hexp_port_state_t.mode) */
 #define HEXP_PORT_MODE_WR_MASTER 1
 #define HEXP_PORT_MODE_WR_SLAVE 2
 #define HEXP_PORT_MODE_NON_WR 3
 
+/*
 #define HEXP_PORT_TSC_RISING 1
 #define HEXP_PORT_TSC_FALLING 2
+*/
 
 typedef struct {
+  /* When non-zero: port state is valid */
   int valid;
+
+  /* WR-PTP role of the port (Master, Slave, etc.) */
   int mode;
-	
-  uint64_t delta_tx;
-  uint64_t delta_rx;
-  uint64_t phase_val;
+
+  /* TX and RX delays (combined, big Deltas from the link model in the spec) */
+  uint32_t delta_tx;
+  uint32_t delta_rx;
+  
+  /* DDMTD raw phase value in picoseconds */
+  uint32_t phase_val;
+
+  /* When non-zero: phase_val contains a valid phase readout */
   int phase_val_valid;
 
+  /* When non-zero: link is up */
   int up;
+
+  /* When non-zero: TX path is calibrated (delta_tx contains valid value) */
   int tx_calibrated;
+
+  /* When non-zero: RX path is calibrated (delta_rx contains valid value) */
   int rx_calibrated;
   int tx_tstamp_counter;
   int rx_tstamp_counter;
@@ -90,8 +112,8 @@ typedef struct {
 
   uint32_t t4_phase_transition; // approximate phase value (on master port) at which RX timestamp (T4) counter transistion occurs (picoseconds)
 
-	uint8_t hw_addr[6];
-	int hw_index;
+  uint8_t hw_addr[6];
+  int hw_index;
 } hexp_port_state_t;
 
 typedef struct {
@@ -99,6 +121,12 @@ typedef struct {
   char port_names[HAL_MAX_PORTS][16];
 } hexp_port_list_t;
 
+typedef struct {
+	int ki, kp;
+	int pll;
+	int branch;
+
+} hexp_pll_cmd_t;
 
 int halexp_check_running();
 int halexp_reset_port(const char *port_name);
@@ -107,6 +135,8 @@ int halexp_lock_cmd(const char *port_name, int command, int priority);
 int halexp_query_ports(hexp_port_list_t *list);
 int halexp_get_port_state(hexp_port_state_t *state, const char *port_name);
 int halexp_pps_cmd(int cmd, hexp_pps_params_t *params);
+int halexp_pll_set_gain(int pll, int branch, int kp, int ki);
+
 
 #endif
 
