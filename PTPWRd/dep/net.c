d505a246ac22399eb56db94590eeff12fc1ad6fe
Tomasz Wlostowski <tomasz.wlostowski@cern.ch>
2011-05-09 18:09:04 +0200

    dep/net.c: removed unused code, new debug api

--- PTPWRd/dep/net.c
+++ PTPWRd/dep/net.c
@@ -4,11 +4,10 @@
 Here we have some intermediate layer between platform-independent *PTPd* and *ptpd_netif* library
 It's not entirely necessary, it is a consequence of the fact that PTPd was adapted from opensource PTPd
 
-
 */
 
 
-#include "../ptpd.h"
+#include "ptpd.h"
 
 #include "ptpd_netif.h"
 #include "hal_exports.h"
@@ -19,80 +18,18 @@ const mac_addr_t ZERO_ADDR          = {0x00, 0x00, 0x00, 0x00, 0x001, 0x00};
 /* shut down the UDP stuff */
 Boolean netShutdown(NetPath *netPath)
 {
-
-  /*  PERROR("WR: not implemented: %s\n", __FUNCTION__ ); */
+  ptpd_netif_close_socket(netPath->wrSock);
+  ptpd_netif_init(); /* fixme */
+    
   return TRUE;
-
-#if 0
-//original implementation
-  struct ip_mreq imr;
-
-  /*Close General Multicast*/
-  imr.imr_multiaddr.s_addr = netPath->multicastAddr;
-  imr.imr_interface.s_addr = htonl(INADDR_ANY);
-
-  setsockopt(netPath->eventSock, IPPROTO_IP, IP_DROP_MEMBERSHIP, &imr, sizeof(struct ip_mreq));
-  setsockopt(netPath->generalSock, IPPROTO_IP, IP_DROP_MEMBERSHIP, &imr, sizeof(struct ip_mreq));
-
-  /*Close Peer Multicast*/
-  imr.imr_multiaddr.s_addr = netPath->peerMulticastAddr;
-  imr.imr_interface.s_addr = htonl(INADDR_ANY);
-
-  setsockopt(netPath->eventSock, IPPROTO_IP, IP_DROP_MEMBERSHIP, &imr, sizeof(struct ip_mreq));
-  setsockopt(netPath->generalSock, IPPROTO_IP, IP_DROP_MEMBERSHIP, &imr, sizeof(struct ip_mreq));
-
-
-  netPath->multicastAddr = 0;
-  netPath->unicastAddr = 0;
-  netPath->peerMulticastAddr = 0;
-
-  /*Close sockets*/
-  if(netPath->eventSock > 0)
-    close(netPath->eventSock);
-  netPath->eventSock = -1;
-
-  if(netPath->generalSock > 0)
-    close(netPath->generalSock);
-  netPath->generalSock = -1;
-
-  return TRUE;
-#endif
-
 }
 
 /*Test if network layer is OK for PTP*/
 UInteger8 lookupCommunicationTechnology(UInteger8 communicationTechnology)
 {
-
-
-  PERROR("WR: not implemented: %s\n", __FUNCTION__ );
+  PTPD_TRACE(TRACE_ERROR, "WR: not implemented: %s\n", __FUNCTION__ );
   return PTP_DEFAULT;
-
-//original implementation
-#if 0
-
-#if defined(linux)
-
-  switch(communicationTechnology)
-  {
-  case ARPHRD_ETHER:
-  case ARPHRD_EETHER:
-  case ARPHRD_IEEE802:
-    return PTP_ETHER;
-
-  default:
-    break;
-  }
-
-#elif defined(BSD_INTERFACE_FUNCTIONS)
-
-#endif
-
-  return PTP_DEFAULT;
-#endif
 }
-
-
 
 Boolean netStartup()
 {
@@ -115,13 +52,12 @@ Boolean netInit(NetPath *netPath, RunTimeOpts *rtOpts, PtpClock *ptpClock)
   wr_sockaddr_t bindaddr;
 
 
-
   if(rtOpts->ifaceName[ptpClock->portIdentity.portNumber - 1][0] != '\0')
   {
     /*interface specified at PTPd start*/
     strcpy(bindaddr.if_name, rtOpts->ifaceName[ptpClock->portIdentity.portNumber - 1]);		// TODO: network intarface
 
-    DBG("Network interface : %s\n",rtOpts->ifaceName[ptpClock->portIdentity.portNumber - 1]  );
+    PTPD_TRACE(TRACE_NET, "Network interface : %s\n",rtOpts->ifaceName[ptpClock->portIdentity.portNumber - 1]  );
   }
   else
   {
@@ -131,15 +67,15 @@ Boolean netInit(NetPath *netPath, RunTimeOpts *rtOpts, PtpClock *ptpClock)
     if(  ptpd_netif_get_ifName(bindaddr.if_name,ptpClock->portIdentity.portNumber ) == PTPD_NETIF_ERROR )
     {
       strcpy(bindaddr.if_name,"wru1");		// TODO: network intarface
-      DBG("Network interface forced to be wru1, but none of the WR ports seems to be up \n");
+      PTPD_TRACE(TRACE_NET, "Network interface forced to be wru1, but none of the WR ports seems to be up \n");
     }
     else
-      DBG("Network interface retrieved automatically by ptpd_netif: %s\n",bindaddr.if_name);
+      PTPD_TRACE(TRACE_NET, "Network interface retrieved automatically by ptpd_netif: %s\n",bindaddr.if_name);
   }
 
   strncpy(netPath->ifaceName,bindaddr.if_name,IFACE_NAME_LENGTH);
 
-  DBG("Network interface : %s\n",netPath->ifaceName);
+  PTPD_TRACE(TRACE_NET, "Network interface : %s\n",netPath->ifaceName);
 
   bindaddr.family = PTPD_SOCK_RAW_ETHERNET;	// socket type
   bindaddr.ethertype = 0x88f7; 	        // PTPv2
@@ -150,7 +86,7 @@ Boolean netInit(NetPath *netPath, RunTimeOpts *rtOpts, PtpClock *ptpClock)
 
   if(netPath->wrSock ==  NULL)
   {
-    PERROR("failed to initalize sockets");
+    PTPD_TRACE(TRACE_ERROR, "failed to initalize sockets");
     return FALSE;
   }
 
@@ -174,7 +110,7 @@ Boolean netInit(NetPath *netPath, RunTimeOpts *rtOpts, PtpClock *ptpClock)
   /* copy mac part to uuid */
   memcpy(ptpClock->port_uuid_field,portMacAddress, PTP_UUID_LENGTH);
 
-  DBG("[%s] mac: %x:%x:%x:%x:%x:%x\n",__func__,\
+  PTPD_TRACE(TRACE_NET, "[%s] mac: %x:%x:%x:%x:%x:%x\n",__func__,\
     ptpClock->port_uuid_field[0],\
     ptpClock->port_uuid_field[1],\
     ptpClock->port_uuid_field[2],\
@@ -187,25 +123,25 @@ Boolean netInit(NetPath *netPath, RunTimeOpts *rtOpts, PtpClock *ptpClock)
   halexp_get_port_state(&pstate, netPath->ifaceName);
 
 
-  DBG(" netif_WR_mode = %d\n", pstate.mode);
+  PTPD_TRACE(TRACE_NET, " netif_WR_mode = %d\n", pstate.mode);
    if(rtOpts->wrNodeMode == NON_WR)
    {
      switch(pstate.mode)
      {
 	case HEXP_PORT_MODE_WR_MASTER:
-	   DBG("wrNodeMode(auto config) ....... MASTER\n");
+	   PTPD_TRACE(TRACE_NET, "wrNodeMode(auto config) ....... MASTER\n");
 	   ptpClock->wrNodeMode = WR_MASTER;
 	   //tmp solution
 	   break;
 	case HEXP_PORT_MODE_WR_SLAVE:
-	   DBG("wrNodeMode(auto config) ........ SLAVE\n");
+	   PTPD_TRACE(TRACE_NET, "wrNodeMode(auto config) ........ SLAVE\n");
 	   ptpClock->wrNodeMode = WR_SLAVE;
 	   ptpd_init_exports();
 	   //tmp solution
 	   break;
 	case HEXP_PORT_MODE_NON_WR:
 	default:
-	   DBG("wrNodeMode(auto config) ........ NON_WR\n");
+	   PTPD_TRACE(TRACE_NET, "wrNodeMode(auto config) ........ NON_WR\n");
 	   ptpClock->wrNodeMode = NON_WR;
 	   //tmp solution
 	   break;
@@ -213,11 +149,11 @@ Boolean netInit(NetPath *netPath, RunTimeOpts *rtOpts, PtpClock *ptpClock)
    }else
    {
      ptpClock->wrNodeMode = rtOpts->wrNodeMode;
-     DBG("wrNodeMode (............ FORCE ON STARTUP\n");
+     PTPD_TRACE(TRACE_NET, "wrNodeMode (............ FORCE ON STARTUP\n");
    }
 
 
-  DBG("netInit: exiting OK\n");
+  PTPD_TRACE(TRACE_NET, "netInit: exiting OK\n");
 
   return TRUE;
 
@@ -255,7 +191,7 @@ ssize_t netRecvMsg(Octet *buf, NetPath *netPath, wr_timestamp_t *current_rx_ts)
 
   if((ret = ptpd_netif_recvfrom(netPath->wrSock, &from_addr, buf, 1518, current_rx_ts)) > 0)
   {
-    //DBG("RX timestamp %s [ret=%d]\n", format_wr_timestamp(*current_rx_ts), ret);
+    //PTPD_TRACE(TRACE_NET, "RX timestamp %s [ret=%d]\n", format_wr_timestamp(*current_rx_ts), ret);
   }
   return (ssize_t)ret;
 
@@ -271,7 +207,7 @@ ssize_t netSendEvent(Octet *buf, UInteger16 length, NetPath *netPath, wr_timesta
   ret = ptpd_netif_sendto(netPath->wrSock, &netPath->multicastAddr, buf, length, current_tx_ts);
 
   if(ret <= 0)
-    DBGNPI("error sending multi-cast event message\n");
+    PTPD_TRACE(TRACE_ERROR, "error sending multi-cast event message\n");
 
   return (ssize_t)ret;
 
@@ -287,7 +223,7 @@ ssize_t netSendGeneral(Octet *buf, UInteger16 length, NetPath *netPath)
 
 
   if(ret <= 0)
-    DBGNPI("error sending multi-cast event message\n");
+    PTPD_TRACE(TRACE_ERROR, "error sending multi-cast event message\n");
 
   return (ssize_t)ret;
 
@@ -303,7 +239,7 @@ ssize_t netSendPeerGeneral(Octet *buf,UInteger16 length,NetPath *netPath)
   ret = ptpd_netif_sendto(netPath->wrSock, &(netPath->multicastAddr), buf, length, NULL);
 
   if(ret <= 0)
-    DBGNPI("error sending multi-cast general message\n");
+    PTPD_TRACE(TRACE_ERROR, "error sending multi-cast general message\n");
 
   return (ssize_t)ret;
 
@@ -318,7 +254,7 @@ ssize_t netSendPeerEvent(Octet *buf,UInteger16 length,NetPath *netPath,wr_timest
   // ret = ptpd_netif_sendto(netPath->wrSock, &(netPath->multicastAddr), buf, length, current_tx_ts);
 
   if(ret <= 0)
-    DBGNPI("error sending multi-cast event message\n");
+    PTPD_TRACE(TRACE_ERROR, "error sending multi-cast event message\n");
 
   return (ssize_t)ret;
 
